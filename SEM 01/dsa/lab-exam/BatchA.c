002a153f7af55accc6305573fe756a01c3a4c244
~/antony/ashish <72491974+ashishantonytsr@users.noreply.github.com>
2024-10-08 11:48:51 +0530

    ```add: dsa-lab-exam batchA program```

---
+++ SEM 01/dsa/lab-exam/BatchA.c
@@ -0,0 +1,88 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node {
+    int value;
+    struct node* next;
+} *start = NULL, * end = NULL;
+
+void insert(int data) {
+    struct node* temp = (struct node*)malloc(sizeof(struct node));
+    temp->value = data;
+    temp->next = NULL;
+
+    if (end == NULL)
+        start = end = temp;
+    else {
+        end->next = temp;
+        end = temp;
+    }
+}
+
+void delete(int data) {
+    int is_found = 0;
+    struct node* curr_node = start;
+    struct node* prev_node = start;
+
+    while (curr_node != NULL) {
+        if (curr_node->value == data) {
+            is_found = 1;
+            if (curr_node == start)
+                start = start->next;
+            else
+                prev_node->next = curr_node->next;
+        }
+
+        prev_node = curr_node;
+        curr_node = curr_node->next;
+    }
+
+    if (is_found == 0) printf("Element not found.\n");
+    else printf("Deleted.\n");
+}
+
+void search(int data) {
+    for (struct node* temp = start; temp != NULL; temp = temp->next) {
+        if (temp->value == data) {
+            printf("Element found.\n");
+            return;
+        }
+    }
+    printf("Element not found.\n");
+}
+
+void display() {
+    if (end == NULL)
+        printf("List is empty.\n");
+    else {
+        printf("The list is: ");
+        for (struct node* temp = start; temp != NULL; temp = temp->next)
+            printf("%d ", temp->value);
+        printf("\n");
+    }
+}
+
+void main() {
+    int n, element;
+    
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+
+    printf("Enter the elements\n");
+    for (int i=0; i<n; i++){
+        scanf("%d", &element);
+        insert(element);
+    }
+
+    display();
+
+    printf("Enter the element to be searched for: ");
+    scanf("%d", &element);
+    search(element);
+
+    printf("Enter the element to be deleted: ");
+    scanf("%d", &element);
+    delete(element);
+
+    display();
+}
