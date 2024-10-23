c31de61bb385ebf760a66405e5d1bcc6f3bad566
~/antony/ashish <72491974+ashishantonytsr@users.noreply.github.com>
2024-10-23 20:04:09 +0530

    ```update: added comments```

--- SEM 01/dsa/assignment/5.c
+++ SEM 01/dsa/assignment/5.c
@@ -6,6 +6,7 @@
 int heap[max], size = 0;
 
 void swap(int *a, int *b){
+	/* swap function using call by reference method */
 	int temp = *a;
 	*a = *b;
 	*b = temp;
@@ -22,19 +23,18 @@ void insert(int value){
 	heap[size++] = value;
 
 	// fixing heap property if it is violated
-	for (int i=size-1; i>0 && heap[(i-1)/2] < heap[i]; i=(i-1)/2){
+	for (int i = size - 1; i > 0 && heap[(i - 1) / 2] < heap[i]; i = (i - 1) / 2)
 		// if parent < current node, swap values
-		int temp = heap[i];
-		heap[i] = heap[(i-1)/2];
-		heap[(i-1)/2] = temp;
-	}
+		swap(&heap[i], &heap[(i - 1) / 2]);
 }
 
 void heapify(){
+	/* Traverse thru the heap array and check whether the elements following heap property */
+
 	for(int i=0; i<size; i++) {
-		int left = 2*i + 1;
-		int right = 2*i + 2;
-		int parent = (i-1)/2;
+		int left = 2 * i + 1; // left child of current node
+		int right = 2 * i + 2; // right child of current node
+		int parent = (i - 1) / 2; // parent of current node
 
 		if (heap[i] > heap[parent])
 			swap(&heap[i], &heap[parent]);
@@ -62,7 +62,7 @@ void delete_at_index(int index){
 	heap[index] = heap[size-1];
 	size--;
 
-	// to ensure heap property
+	// to ensure heap property is maintained
 	heapify();
 }
 
