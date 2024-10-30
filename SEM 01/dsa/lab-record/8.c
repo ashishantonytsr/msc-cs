76c17f2e385e9bfbb51224a71bd0839d60e3d549
~/antony/ashish <72491974+ashishantonytsr@users.noreply.github.com>
2024-10-30 08:26:00 +0530

    ```update: dsa lab pgrm 08```

--- SEM 01/dsa/lab-record/8.c
+++ SEM 01/dsa/lab-record/8.c
@@ -1,7 +1,8 @@
-// BST
+// Binary Search Tree
 
 #include <stdio.h>
-#include <stdlib.h>
+#include <stdlib.h> 
+#include <stdbool.h>
 
 struct node {
     int value;
@@ -9,91 +10,100 @@ struct node {
     struct node* right;
 };
 
-struct node* insert_node(struct node* root, int value) {
+struct node* insert(struct node* root, int value) {
     if (root == NULL) {
-        struct node* temp = (struct node*)malloc(sizeof(struct node));
-        temp->value = value;
-        temp->left = temp->right = NULL;
-        return temp;
+        struct node* new_node = (struct node*)malloc(sizeof(struct node));
+        new_node->value = value;
+        new_node->left = new_node->right = NULL;
+        return new_node;
     }
 
+    // recursively traversing left & right subtrees
     if (value < root->value)
-        root->left = insert_node(root->left, value);
+        root->left = insert(root->left, value);
     else
-        root->right = insert_node(root->right, value);
+        root->right = insert(root->right, value);
 
     return root;
 }
 
-struct node* delete_node(struct node* root, int value) {
-    if (root == NULL) return;
+struct node* delete(struct node* root, int value) {
+    if (root == NULL) return root;
 
-    // Find the node to be deleted
+    // traversing the tree to find the node to be deleted
     if (value < root->value)
-        root->left = delete_node(root->left, value);
+        root->left = delete(root->left, value);
     else if (value > root->value)
-        root->right = delete_node(root->right, value);
+        root->right = delete(root->right, value);
 
+    // found the node to be deleted
     else {
-        // case 1: Node with one or zero children
-        // return the non-null child node
+
+        // root has only right child
         if (root->left == NULL) {
             struct node* temp = root->right;
             free(root);
             return temp;
         }
-        else if (root->right == NULL) {
+
+        // root has only left child
+        if (root->right == NULL) {
             struct node* temp = root->left;
             free(root);
             return temp;
         }
 
-        // case 2: Node with two children
-        // find min-value node in right subtree
-        struct node* min_node = root;
-        for (; min_node->left != NULL; min_node = min_node->left);
+        // root has two children
+        // ---
+        // finding successor of root
+        struct node* next = root->right;
+        for (; next != NULL && next->left != NULL; next = next->left);
 
-        // swap values of min-value node and root node
-        int temp = min_node->value;
-        min_node->value = root->value;
-        root->value = temp;
-
-        // remove the min-value node
-        free(min_node);
+        // update root with value of successor node
+        root->value = next->value;
+        // delete successor node recursively
+        root->right = delete(root->right, next->value);
     }
     return root;
 }
 
-// In-order tree traversal
-void display(struct node* root) {
-    if (root == NULL) return;
-    display(root->left);
-    printf("->%d ", root->value);
-    display(root->right);
+bool search(struct node* root, int value) {
+    if (root == NULL) return false;
+
+    if (value < root->value)
+        search(root->left, value);
+    else if (value > root->value)
+        search(root->right, value);
+    return true;
 }
+
+void inorder_traversal(struct node* root) {
+    if (root != NULL) {
+        inorder_traversal(root->left);
+        printf("%d ", root->value);
+        inorder_traversal(root->right);
+    }
+}
+
 
 void main() {
     struct node* root = NULL;
 
-    root = insert_node(root, 12);
-    root = insert_node(root, 23);
-    root = insert_node(root, 89);
-    root = insert_node(root, 78);
-    root = insert_node(root, 34);
-    root = insert_node(root, 45);
-    root = insert_node(root, 56);
-    display(root);
+    root = insert(root, 5);
+    root = insert(root, 8);
+    root = insert(root, 3);
+    root = insert(root, 9);
+    root = insert(root, 7);
+    root = insert(root, 11);
+    root = insert(root, 12);
+    root = insert(root, 16);
+    root = insert(root, 6);
+    inorder_traversal(root);
     printf("\n");
-    root = delete_node(root, 34);
-    display(root);
-    printf("\n");
-    root = delete_node(root, 89);
-    display(root);
-    printf("\n");
-    root = delete_node(root, 12);
-    display(root);
-    printf("\n");
-    root = delete_node(root, 56);
-    display(root);
-    printf("\n");
+
+    search(root, 11) ? printf("Item found\n") : printf("Item not found\n");
+    search(root, 18) ? printf("Item found\n") : printf("Item not found\n");
+
+    root = delete(root, 8);
+    inorder_traversal(root);
 }
